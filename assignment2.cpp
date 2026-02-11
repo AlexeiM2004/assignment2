177c31c7f97b9668b8e14ff9f6b79aa995559ba8
AlexeiM2004 <alexei.maiorov@student.manchester.ac.uk>
2026-02-11 10:26:28 +0000

    Started to create the skeleton code with initial commented ideas for this project.

---
+++ assignment2.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cmath>
+#include <fstream>
+#include <sstream>
+
+///Function to calculate mean, std, variance, maybe a skibidy graph
+
+int exam_statistics_calculator()
+{
+    //Here we will calculate, mean, mode and range
+    //Here we will calculate standard deviation & variance
+    //Possibly plot a distribution of exam results
+}
+
+//Function to read in file, state how many entries there are, organise, write into new file
+
+int placeholder_function_name_1()
+{
+    //Define constants
+    //Read in a file
+    //State how many entries it has read, (for loops)
+    //Ask the user questions?
+    //Should be able to filter by course year, and by course name
+
+}
+
+int main(){
+    exam_statistics_calculator();
+    placeholder_function_name_1();
+
+    return 0;
+}
