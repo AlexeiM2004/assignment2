a9a12e068a381785f9b9ef81987701bac49296d0
AlexeiM2004 <alexei.maiorov@student.manchester.ac.uk>
2026-02-16 13:19:55 +0000

    Wrote a new pseudocode layout out my program to include a new main menu, by which the user can more easily interface. From the main menu the user can access 3 different options and of course an exit code. They can then enter different display functions to analyse / see different data based on year, or (M) etc... , this layout should now only require a few "Worker functions" such as statistical calculator, filter and sorter.

--- assignment2.cpp
+++ assignment2.cpp
@@ -4,6 +4,7 @@
 #include <cmath>
 #include <fstream>
 #include <sstream>
+#include <limits>
 
 //Structure to contain the information 
 
@@ -15,21 +16,10 @@ struct course_information
     std::string course_module_name;
 };
 
-///Function to calculate mean, std, variance, maybe a skibidy graph
-
-void exam_statistics_calculator()
-{
-    //Here we will calculate, mean, mode and range
-    //Here we will calculate standard deviation & variance
-    //Possibly plot a distribution of exam results
-}
-
 //Function to read in file, state how many entries there are, organise, write into new file
 
-void placeholder_function_name_1()
+std::vector<course_information> load_exam_statistics()
 {
-    //Define constants
-
     //Read in a file and create arrays containing the file's parameters
 
     std::ifstream file("C:\\Users\\lexma\\projects\\assignment2\\course_marks.dat");
@@ -56,22 +46,125 @@ void placeholder_function_name_1()
             }
         }     
     }
-    //State how many entries it has read, (for loops)
-    //Ask the user questions?
-    //Should be able to filter by course year, and by course name
-    std::cout << "Read in " << courses.size() << " Course records\n";
-    for(int i=0; i < courses.size(); ++i)
+    std::cout << "Successfully read in " << courses.size() << " exam records\n";
+    return courses;
+}
+
+//----------Statistical calculation function----------//
+
+double exam_statistics_calculator(const std::vector<course_information>& courses)
+{
+    return 0;
+}
+
+//----------Filtering functions----------//
+
+std::vector<course_information> exam_statistics_filter_by_year(const std::vector<course_information>& courses, int year)
+{
+    std::vector<course_information> user_selected_course_year;
+    for(const auto& course : courses)
     {
-        std::cout << "\nCourse Mark " << courses[i].course_mark;
-        std::cout << " Course Year " << courses[i].course_year;
-        std::cout << " Course Tag " << courses[i].course_module_tag;
-        std::cout << " Course Name " << courses[i].course_module_name;
+        if(course.course_year == year){
+            user_selected_course_year.push_back(course);
+        }
+    }
+    return user_selected_course_year;
+}
+
+//----------Display functions----------//
+
+//Function to allow user to view all course information, or course information by year
+
+void display_bulk_statistics(const std::vector<course_information>& courses)
+{
+    std::cout << "User is in the display bulk statistics function";
+}
+
+//Function to display all calculated year's course statistics
+
+void display_all_course_statistics(const std::vector<course_information>& courses)
+{
+    std::cout << "User is in the display all course statistics function";
+}
+
+//Function to display a selected year's course statistics
+
+void display_course_statistics_by_year(const std::vector<course_information>& courses)
+{
+    std::cout << "User is in the display course statistics by year function";
+}
+
+//Old function to be reworked into the code
+
+void user_interface(const std::vector<course_information>& courses)
+{
+    //Asks the user what course year information they would like to view
+    std::cout << "\nPlease enter a course year you would like to view: ";
+    int year;
+    std::cin >> year;
+    while(year <= 0 || year > 4 )
+    {
+        std::cout << "\nInvalid course year input, please enter a course between the years 1-4: ";
+        std::cin >> year;
+    }
+    
+    //Call the exam filter by year function
+
+    std::vector<course_information> user_selected_exam_year_information = exam_statistics_filter_by_year(courses, year);
+    std::cout << "\nIdentified " << user_selected_exam_year_information.size() << " courses in year " << year << "\n";
+    for(const auto& course : user_selected_exam_year_information)
+    {
+        std::cout << "\nModule name : " << course.course_module_name;
+        std::cout << "\nModule module tag : " << course.course_module_tag;
+        std::cout << "\nModule mark : " << course.course_mark;
+
+    }
+
+}
+
+//Main menu, containing paths to all other functions
+
+void main_menu(const std::vector<course_information>& courses)
+{
+    std::cout << "\nWelcome to the course marks data program.";
+    std::cout << "\nWhat would you like to do today? ";
+    std::cout << "\nOption 1 - View bulk course mark data.";
+    std::cout << "\nOption 2 - View all year's exam statistics.";
+    std::cout << "\nOption 3 - View a selected year's exam statistics.";
+    std::cout << "\nOption 4 - Exit course marks data program.";
+    std::cout << "\nPlease select an option 1-4: ";
+    int main_menu_option = 0;
+    std::cin >> main_menu_option;
+    while(std::cin.fail() || main_menu_option <= 0 || main_menu_option > 4)
+    {
+    std::cout << "\nYou have entered an invalid input, please select an option 1-4:";
+    std::cin.clear();
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    std::cin >> main_menu_option;
+    }
+    if(main_menu_option == 1)
+    {
+        display_bulk_statistics(courses);
+    }else if (main_menu_option == 2)
+    {
+        display_all_course_statistics(courses);
+    }else if (main_menu_option == 3)
+    {
+        display_course_statistics_by_year(courses);
+    }else if (main_menu_option == 4)
+    {
+        std::cout << "\nThank you for using this program, have a lovely day.";
+        //Find a backdoor for the program to escape here
     }
 }
 
+//----------Main function, No touchy----------//
+
 int main(){
-    exam_statistics_calculator();
-    placeholder_function_name_1();
+    std::vector<course_information> courses = load_exam_statistics();
+    main_menu(courses);
+    //exam_statistics_calculator(courses);
+    //user_interface(courses);
 
     return 0;
 }
